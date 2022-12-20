f3c9b9cbe834014cebca0bc930d8ad998e4b3fa5
arno67000 <arno68.as@gmail.com>
2022-12-21 00:22:52 +0100

    :art: adding some style and new font

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 "use strict";
 class Game {
-    constructor(level, canva, timer, startButton, scoreArea, alertArea) {
+    constructor(level, canva, timer, startButton, scoreArea, alertArea, levelArea, resultArea, accuracyArea) {
         this.running = false;
         this.level = level;
-        this.time = 59 + level;
+        this.time = 60;
         this.breakpoint = 51 - level;
         this.delay = 3000 - level * level;
         this.score = 0;
@@ -15,6 +15,11 @@ class Game {
         this.timer = timer;
         this.startButton = startButton;
         this.scoreArea = scoreArea;
+        this.clicked = 0;
+        this.missed = 0;
+        this.levelArea = levelArea;
+        this.resultArea = resultArea;
+        this.accuracyArea = accuracyArea;
     }
     prepare() {
         this.disableAlert();
@@ -25,38 +30,53 @@ class Game {
                 targetElement.classList.contains("virus")) {
                 targetElement.remove();
                 this.score++;
+                this.clicked++;
                 this.scoreArea.textContent = String(this.score);
+            }
+            else {
+                this.running && this.missed++;
             }
         });
         this.startButton.addEventListener("click", () => {
-            if (!this.running && this.won)
+            if (!this.running && this.won) {
                 this.level++;
-            !this.running && this.start();
+            }
+            else if (!this.running && this.over) {
+                this.level = 1;
+            }
+            !this.running && this.start(this.won);
         });
     }
-    start() {
+    start(keepScore) {
         if (!this.running) {
-            this.running = true;
-            this.over = false;
-            this.disableStartButton();
+            this.setupNewGame(keepScore);
         }
         else {
             alert("game is already running");
         }
-        this.enableAlert();
         this.run();
     }
     run() {
         this.over = this.gameOver();
-        if (this.time === 0 && !this.over) {
-            // handle score && message et button next wave
+        if (this.over) {
+            this.running = false;
+            this.over = true;
+            this.disableAlert();
+            this.enableStartButton(this.won);
+            this.enableResults(this.won);
+            this.screenCleanUp();
+        }
+        else if (this.time === 0) {
             this.running = false;
             this.won = true;
-            this.enableStartButton();
-            alert("you win");
+            this.disableAlert();
+            this.enableStartButton(this.won);
+            this.enableResults(this.won);
         }
-        else if (!this.over) {
-            const invaders = this.level % 2 ? this.level / 2 : Math.round(this.level % 2);
+        else {
+            const invaders = this.level % 2 === 0
+                ? this.level / 2
+                : Math.ceil(this.level / 2);
             for (let i = 0; i < invaders; i++) {
                 this.addVirus();
             }
@@ -65,12 +85,6 @@ class Game {
                 this.timer.textContent = String(this.time);
                 this.run();
             }, this.delay);
-        }
-        else {
-            // handle score && message et button start
-            this.running = false;
-            this.over = true;
-            alert("game over");
         }
     }
     addVirus() {
@@ -93,17 +107,57 @@ class Game {
     gameOver() {
         return (this.canva.getElementsByClassName("virus").length >= this.breakpoint);
     }
+    screenCleanUp() {
+        const viruses = Object.values(this.canva.getElementsByClassName("virus"));
+        viruses.forEach((element) => {
+            setTimeout(() => {
+                element.remove();
+            }, 550);
+        });
+    }
+    setupNewGame(keepScore) {
+        this.time = 60;
+        this.breakpoint = 51 - this.level;
+        this.delay = 3000 - this.level * this.level;
+        this.clicked = 0;
+        this.missed = 0;
+        this.running = true;
+        this.over = false;
+        this.won = false;
+        this.levelArea.innerText = String(this.level);
+        this.disableStartButton();
+        this.disableResults();
+        this.enableAlert();
+        if (!keepScore) {
+            this.score = 0;
+        }
+    }
     enableAlert() {
         this.alertArea.removeAttribute("hidden");
     }
     disableAlert() {
         this.alertArea.setAttribute("hidden", "true");
     }
-    enableStartButton() {
-        this.startButton.removeAttribute("disabled");
+    enableStartButton(won) {
+        this.startButton.innerHTML = won ? "Start next level" : "Start level";
+        this.startButton.removeAttribute("hidden");
     }
     disableStartButton() {
-        this.startButton.setAttribute("disabled", "true");
+        this.startButton.setAttribute("hidden", "true");
+    }
+    enableResults(won) {
+        const rating = (this.clicked * 100) / (this.clicked + this.missed);
+        this.accuracyArea.innerText = `Accuracy: ${String(rating)} %`;
+        this.resultArea.innerText = won ? "You WIN" : "You LOOSE";
+        this.resultArea.style.color = won ? "aqua" : "red";
+        this.accuracyArea.style.color =
+            rating > 75 ? "aquamarine" : rating > 50 ? "orange" : "red";
+        this.accuracyArea.removeAttribute("hidden");
+        this.resultArea.removeAttribute("hidden");
+    }
+    disableResults() {
+        this.resultArea.setAttribute("hidden", "true");
+        this.accuracyArea.setAttribute("hidden", "true");
     }
 }
 // Randomizers
@@ -127,12 +181,22 @@ function isHTMLElement(el) {
 (() => {
     const startButton = document.getElementById("start");
     const canva = document.getElementById("canva");
-    const score = document.getElementById("score");
-    const time = document.getElementById("time");
+    const scoreArea = document.getElementById("score");
+    const timerArea = document.getElementById("time");
     const alertArea = document.getElementById("alert");
-    if (!canva || !time || !score || !startButton || !alertArea) {
+    const levelArea = document.getElementById("level");
+    const resultArea = document.getElementById("result");
+    const accuracyArea = document.getElementById("accuracy");
+    if (!canva ||
+        !timerArea ||
+        !scoreArea ||
+        !startButton ||
+        !alertArea ||
+        !levelArea ||
+        !resultArea ||
+        !accuracyArea) {
         alert("Missing element in page, please reload and try again.");
         return;
     }
-    new Game(1, canva, time, startButton, score, alertArea).prepare();
+    new Game(1, canva, timerArea, startButton, scoreArea, alertArea, levelArea, resultArea, accuracyArea).prepare();
 })();
