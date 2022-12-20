091669acfccf6c3d30a6c3e0acc9f9801b60ee50
arno67000 <arno68.as@gmail.com>
2022-12-21 00:23:12 +0100

    :sparkles: improving game logic

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
 class Game {
-    time: number;
-    breakpoint: number;
-    delay: number;
-    score: number;
-    alertArea: HTMLElement;
-    canva: HTMLElement;
-    timer: HTMLElement;
-    startButton: HTMLElement;
-    scoreArea: HTMLElement;
-    level: number;
+    private accuracyArea: HTMLElement;
+    private alertArea: HTMLElement;
+    private breakpoint: number;
+    private canva: HTMLElement;
+    private clicked: number;
+    private delay: number;
+    private level: number;
+    private levelArea: HTMLElement;
+    private missed: number;
     private over: boolean;
-    private won: boolean;
+    private resultArea: HTMLElement;
     private running = false;
+    private score: number;
+    private scoreArea: HTMLElement;
+    private startButton: HTMLElement;
+    private time: number;
+    private timer: HTMLElement;
+    private won: boolean;
+
     constructor(
         level: number,
         canva: HTMLElement,
         timer: HTMLElement,
         startButton: HTMLElement,
         scoreArea: HTMLElement,
-        alertArea: HTMLElement
+        alertArea: HTMLElement,
+        levelArea: HTMLElement,
+        resultArea: HTMLElement,
+        accuracyArea: HTMLElement
     ) {
         this.level = level;
-        this.time = 59 + level;
+        this.time = 60;
         this.breakpoint = 51 - level;
         this.delay = 3000 - level * level;
         this.score = 0;
@@ -33,6 +42,11 @@ class Game {
         this.timer = timer;
         this.startButton = startButton;
         this.scoreArea = scoreArea;
+        this.clicked = 0;
+        this.missed = 0;
+        this.levelArea = levelArea;
+        this.resultArea = resultArea;
+        this.accuracyArea = accuracyArea;
     }
 
     prepare() {
@@ -46,38 +60,51 @@ class Game {
             ) {
                 targetElement.remove();
                 this.score++;
+                this.clicked++;
                 this.scoreArea.textContent = String(this.score);
+            } else {
+                this.running && this.missed++;
             }
         });
         this.startButton.addEventListener("click", () => {
-            if (!this.running && this.won) this.level++;
-            !this.running && this.start();
+            if (!this.running && this.won) {
+                this.level++;
+            } else if (!this.running && this.over) {
+                this.level = 1;
+            }
+            !this.running && this.start(this.won);
         });
     }
 
-    start() {
+    start(keepScore: boolean) {
         if (!this.running) {
-            this.running = true;
-            this.over = false;
-            this.disableStartButton();
+            this.setupNewGame(keepScore);
         } else {
             alert("game is already running");
         }
-        this.enableAlert();
         this.run();
     }
 
     run() {
         this.over = this.gameOver();
-        if (this.time === 0 && !this.over) {
-            // handle score && message et button next wave
+        if (this.over) {
+            this.running = false;
+            this.over = true;
+            this.disableAlert();
+            this.enableStartButton(this.won);
+            this.enableResults(this.won);
+            this.screenCleanUp();
+        } else if (this.time === 0) {
             this.running = false;
             this.won = true;
-            this.enableStartButton();
-            alert("you win");
-        } else if (!this.over) {
+            this.disableAlert();
+            this.enableStartButton(this.won);
+            this.enableResults(this.won);
+        } else {
             const invaders =
-                this.level % 2 ? this.level / 2 : Math.round(this.level % 2);
+                this.level % 2 === 0
+                    ? this.level / 2
+                    : Math.ceil(this.level / 2);
             for (let i = 0; i < invaders; i++) {
                 this.addVirus();
             }
@@ -86,11 +113,6 @@ class Game {
                 this.timer.textContent = String(this.time);
                 this.run();
             }, this.delay);
-        } else {
-            // handle score && message et button start
-            this.running = false;
-            this.over = true;
-            alert("game over");
         }
     }
 
@@ -133,6 +155,34 @@ class Game {
         );
     }
 
+    screenCleanUp() {
+        const viruses = Object.values(
+            this.canva.getElementsByClassName("virus")
+        );
+        viruses.forEach((element: Element) => {
+            setTimeout(() => {
+                element.remove();
+            }, 550);
+        });
+    }
+    setupNewGame(keepScore: boolean) {
+        this.time = 60;
+        this.breakpoint = 51 - this.level;
+        this.delay = 3000 - this.level * this.level;
+        this.clicked = 0;
+        this.missed = 0;
+        this.running = true;
+        this.over = false;
+        this.won = false;
+        this.levelArea.innerText = String(this.level);
+        this.disableStartButton();
+        this.disableResults();
+        this.enableAlert();
+        if (!keepScore) {
+            this.score = 0;
+        }
+    }
+
     enableAlert() {
         this.alertArea.removeAttribute("hidden");
     }
@@ -140,11 +190,28 @@ class Game {
         this.alertArea.setAttribute("hidden", "true");
     }
 
-    enableStartButton() {
-        this.startButton.removeAttribute("disabled");
+    enableStartButton(won: boolean) {
+        this.startButton.innerHTML = won ? "Start next level" : "Start level";
+        this.startButton.removeAttribute("hidden");
     }
     disableStartButton() {
-        this.startButton.setAttribute("disabled", "true");
+        this.startButton.setAttribute("hidden", "true");
+    }
+
+    enableResults(won: boolean) {
+        const rating = (this.clicked * 100) / (this.clicked + this.missed);
+        this.accuracyArea.innerText = `Accuracy: ${String(rating)} %`;
+        this.resultArea.innerText = won ? "You WIN" : "You LOOSE";
+
+        this.resultArea.style.color = won ? "aqua" : "red";
+        this.accuracyArea.style.color =
+            rating > 75 ? "aquamarine" : rating > 50 ? "orange" : "red";
+        this.accuracyArea.removeAttribute("hidden");
+        this.resultArea.removeAttribute("hidden");
+    }
+    disableResults() {
+        this.resultArea.setAttribute("hidden", "true");
+        this.accuracyArea.setAttribute("hidden", "true");
     }
 }
 
@@ -172,12 +239,34 @@ function isHTMLElement(el: unknown): el is HTMLElement {
 (() => {
     const startButton = document.getElementById("start");
     const canva = document.getElementById("canva");
-    const score = document.getElementById("score");
-    const time = document.getElementById("time");
+    const scoreArea = document.getElementById("score");
+    const timerArea = document.getElementById("time");
     const alertArea = document.getElementById("alert");
-    if (!canva || !time || !score || !startButton || !alertArea) {
+    const levelArea = document.getElementById("level");
+    const resultArea = document.getElementById("result");
+    const accuracyArea = document.getElementById("accuracy");
+    if (
+        !canva ||
+        !timerArea ||
+        !scoreArea ||
+        !startButton ||
+        !alertArea ||
+        !levelArea ||
+        !resultArea ||
+        !accuracyArea
+    ) {
         alert("Missing element in page, please reload and try again.");
         return;
     }
-    new Game(1, canva, time, startButton, score, alertArea).prepare();
+    new Game(
+        1,
+        canva,
+        timerArea,
+        startButton,
+        scoreArea,
+        alertArea,
+        levelArea,
+        resultArea,
+        accuracyArea
+    ).prepare();
 })();
